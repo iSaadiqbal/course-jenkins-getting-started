49c5bfd2f0f827d1a04539c9db506d4fd36b886c
isaadiqbal <ssadiqbalbutt89@gmail.com>
2023-08-05 08:38:00 +0500

    ola

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -5,7 +5,7 @@ pipeline {
     stages {   
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/iSaadiqbal/jgsu-spring-petclinic.git, branch: 'main' }            
+                git url: 'https://github.com/iSaadiqbal/jgsu-spring-petclinic.git', branch: 'main' }            
         }
         stage('compile') {
             steps {sh './mvnw clean compile'}
