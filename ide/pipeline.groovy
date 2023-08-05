50183175f6d2feb077c6344ac3d44c125413c4aa
isaadiqbal <ssadiqbalbutt89@gmail.com>
2023-08-05 07:59:57 +0500

    lol

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -1,31 +1,33 @@
 pipeline {
     agent any
     triggers { pollSCM('* * * * *') }
-    stages {
+    
+    stages {   
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/iSaadiqbal/course-jenkins-getting-started.git', branch: 'main'
-            }            
+                git url: 'https://github.com/iSaadiqbal/course-jenkins-getting-started.git', branch: 'main' }            
+        }
+        stage('compile') {
+            steps {sh './mvnw clean compile'}
         }
         stage('Build') {
-            steps {
-                sh './mvnw clean package'
-            }
-        
-            post {
+            steps {sh './mvnw clean package'}
+        }
+        stage('test logs') {
+            steps { junit '**/target/surefire-reports/TEST-*.xml'}
+        }
+        stage('download logs') { 
+            steps {archiveArtifacts 'target/*.jar' }
+        }
+    }
+        post {
                 always {
                     emailext (
                        subject: "Job '${JOB_NAME}' (build ${BUILD_NUMBER}) ${currentBuild.result}",
                         body: "Please go to ${BUILD_URL} and verify the build", 
                         attachLog: true, 
-                        compressLog: true, 
                         to: "saad89.linux@gmail.com", 
-                    )
-                        
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts 'target/*.jar'
+                    )     
                 }
             }
-        }
     }
-}
